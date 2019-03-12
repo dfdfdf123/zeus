3b7402499646e66e3dd62b9da84dd0395b9a720c
johnromanoff <johnromanoff@yandex.ru>
2019-03-12 23:31:37 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/js/index.js',
+    './src/scss/style.scss'
+  ],
+  output: {
+    filename: './js/bundle.js'
+  },
+  devtool: "source-map",
+  module: {
+    rules: [{
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: 'env'
+          }
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: ExtractTextPlugin.extract({
+          use: [{
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                url: false
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader']
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: './css/style.bundle.css',
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/html/index.pug'
+    }),
+    new CopyWebpackPlugin([{
+      from: './src/img',
+      to: './img'
+    }
+  ]),
+  ]
+};
